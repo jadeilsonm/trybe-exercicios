5591642089f52e17300532cd732f6cb900ad41a5
Jadeilson Martinho <jms@a.palmares.ifpe.edu.br>
2021-11-21 11:13:24 -0300

    exercicio 5

--- fundamentos/bloco-5-JavaScript:-DOM/exercises_5_3/calendarioTrybe/script.js
+++ fundamentos/bloco-5-JavaScript:-DOM/exercises_5_3/calendarioTrybe/script.js
@@ -21,11 +21,13 @@ function addDias(array) {
     for (const day of array) {
         let tagLi = document.createElement('li');
         tagLi.innerText = day;
-        if (day === 24 || day === 25 || day === 31) {
+        if (day === 24 | day === 31) {
             tagLi.className = 'day holiday';
-        } else if (day === 4 || day === 11 || day === 18 || day === 25) {
+        } else if (day === 4 | day === 11 | day === 18) {
             tagLi.className = 'day friday';
-        } else {
+        }else if (day === 25) {
+            tagLi.className = 'day holiday friday';
+        }else {
             tagLi.className = 'day';
         }
         document.querySelector('#days').appendChild(tagLi)
@@ -33,11 +35,39 @@ function addDias(array) {
 }
 addDias(dezDaysList);
 
-function creeatBtn(str){
+function creeatBtn(str,id){
     tagBtn = document.createElement('button');
     tagBtn.innerText = str;
-    tagBtn.id = 'btn-holiday';
+    tagBtn.id = id;
     document.querySelector('.buttons-container').appendChild(tagBtn);
 }
 
-creeatBtn('Feriados');
+creeatBtn('Feriados','btn-holiday');
+
+document.querySelector('#btn-holiday').addEventListener('click',feriadosColor);
+function feriadosColor(){
+    let dayFeriados = document.querySelectorAll('.holiday');
+    for (const day of dayFeriados) {
+        if (day.style.backgroundColor === 'green'){
+            day.style.backgroundColor = 'rgb(238,238,238)';
+        }else{
+            day.style.backgroundColor = 'green';
+        }
+    }
+}
+
+creeatBtn('Sexta-feira','btn-friday');
+
+function sextou(){
+    let arrayDay = document.querySelectorAll('.day');
+    for (let i = 0;i < arrayDay.length; i ++) {
+        if(arrayDay[i].innerText === '4' || arrayDay[i].innerText === '11' || arrayDay[i].innerText === '18' || arrayDay[i].innerText === '25'  ){
+            arrayDay[i].innerText = 'Sextou!!';
+        } else if(arrayDay[i].innerText === 'Sextou!!' || arrayDay[i].innerText === 'Sextou!!' || arrayDay[i].innerText === 'Sextou!!' || arrayDay[i].innerText === 'Sextou!!'  ){
+            arrayDay[i].innerText = i - 1;
+        }
+    }
+}
+
+document.querySelector('#btn-friday').addEventListener('click',sextou);
+
