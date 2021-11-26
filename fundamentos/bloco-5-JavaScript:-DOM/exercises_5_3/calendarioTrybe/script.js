6f7705252a34c117209be43c908cd1c9f03059bf
Jadeilson Martinho <jms@a.palmares.ifpe.edu.br>
2021-11-26 16:27:46 -0300

    modificações calendario

--- fundamentos/bloco-5-JavaScript:-DOM/exercises_5_3/calendarioTrybe/script.js
+++ fundamentos/bloco-5-JavaScript:-DOM/exercises_5_3/calendarioTrybe/script.js
@@ -25,9 +25,9 @@ function addDias(array) {
             tagLi.className = 'day holiday';
         } else if (day === 4 | day === 11 | day === 18) {
             tagLi.className = 'day friday';
-        }else if (day === 25) {
+        } else if (day === 25) {
             tagLi.className = 'day holiday friday';
-        }else {
+        } else {
             tagLi.className = 'day';
         }
         document.querySelector('#days').appendChild(tagLi)
@@ -35,39 +35,126 @@ function addDias(array) {
 }
 addDias(dezDaysList);
 
-function creeatBtn(str,id){
+function creeatBtn(str, id) {
     tagBtn = document.createElement('button');
     tagBtn.innerText = str;
     tagBtn.id = id;
     document.querySelector('.buttons-container').appendChild(tagBtn);
 }
 
-creeatBtn('Feriados','btn-holiday');
+creeatBtn('Feriados', 'btn-holiday');
 
-document.querySelector('#btn-holiday').addEventListener('click',feriadosColor);
-function feriadosColor(){
+document.querySelector('#btn-holiday').addEventListener('click', feriadosColor);
+function feriadosColor() {
     let dayFeriados = document.querySelectorAll('.holiday');
     for (const day of dayFeriados) {
-        if (day.style.backgroundColor === 'green'){
+        if (day.style.backgroundColor === 'green') {
             day.style.backgroundColor = 'rgb(238,238,238)';
-        }else{
+        } else {
             day.style.backgroundColor = 'green';
         }
     }
 }
 
-creeatBtn('Sexta-feira','btn-friday');
+creeatBtn('Sexta-feira', 'btn-friday');
 
-function sextou(){
+function sextou() {
     let arrayDay = document.querySelectorAll('.day');
-    for (let i = 0;i < arrayDay.length; i ++) {
-        if(arrayDay[i].innerText === '4' || arrayDay[i].innerText === '11' || arrayDay[i].innerText === '18' || arrayDay[i].innerText === '25'  ){
+    for (let i = 0; i < arrayDay.length; i++) {
+        if (arrayDay[i].innerText === '4' || arrayDay[i].innerText === '11' || arrayDay[i].innerText === '18' || arrayDay[i].innerText === '25') {
             arrayDay[i].innerText = 'Sextou!!';
-        } else if(arrayDay[i].innerText === 'Sextou!!' || arrayDay[i].innerText === 'Sextou!!' || arrayDay[i].innerText === 'Sextou!!' || arrayDay[i].innerText === 'Sextou!!'  ){
+        } else if (arrayDay[i].innerText === 'Sextou!!' || arrayDay[i].innerText === 'Sextou!!' || arrayDay[i].innerText === 'Sextou!!' || arrayDay[i].innerText === 'Sextou!!') {
             arrayDay[i].innerText = i - 1;
         }
     }
 }
 
-document.querySelector('#btn-friday').addEventListener('click',sextou);
+document.querySelector('#btn-friday').addEventListener('click', sextou);
 
+
+
+let days = document.querySelector('#days');
+days.addEventListener('mouseover', function (event) {
+    event.target.style.fontSize = '60px';
+    event.target.style.fontWeight = '600';
+})
+
+days.addEventListener('mouseout', (event) => {
+    event.target.style.fontWeight = '200';
+    event.target.style.fontSize = '20px';
+})
+
+function creatTarefas(str) {
+    let tarefa = document.createElement('span');
+    tarefa.innerText = str;
+    document.querySelector('.my-tasks').appendChild(tarefa);
+}
+creatTarefas('cozinhar')
+
+function legenda(cor) {
+    let legenda = document.createElement('div');
+    legenda.className = 'task';
+    legenda.style.backgroundColor = cor;
+    document.querySelector('.my-tasks').appendChild(legenda);
+}
+
+legenda('red')
+
+document.querySelector('.task').addEventListener('click', (event) => {
+    if (event.target.className === 'task selected') {
+        event.target.className = 'task'
+    } else {
+        event.target.className = 'task selected';
+    }
+})
+
+
+//gabarito do corse
+function setDayColor() {
+    let selectedTask = document.getElementsByClassName('task selected');
+    let days = document.querySelector('#days');
+    let taskDiv = document.querySelector('.task');
+    let taskColor = taskDiv.style.backgroundColor;
+
+    days.addEventListener('click', function (event) {
+        let eventTargetColor = event.target.style.color;
+        if (selectedTask.length > 0 && eventTargetColor !== taskColor) {
+            let color = selectedTask[0].style.backgroundColor;
+            event.target.style.color = color;
+        } else if (eventTargetColor === taskColor && selectedTask.length !== 0) {
+            event.target.style.color = 'rgb(119,119,119)';
+        }
+    });
+};
+
+setDayColor();
+
+
+function compromissos() {
+    let btn = document.querySelector('#btn-add');
+    let input = document.querySelector('#task-input');
+    let list = document.querySelector('.task-list');
+
+    btn.addEventListener('click', () => {
+        if (input.value.length > 0) {
+            let newLi = document.createElement('li');
+            newLi.innerText = input.value;
+            list.appendChild(newLi);
+            input.value = '';
+        } else {
+            alert('Error: Digite ao menos 1 caractere.');
+        }
+
+    })
+    input.addEventListener('keyup',(event) => {
+        if (event.key === 'Enter' && input.value.length > 0) {
+            let newLi = document.createElement('li');
+            newLi.innerText = getInputField.value;
+      
+            list.appendChild(newLi);
+            input.value = '';
+          }
+    })
+}
+
+compromissos();
